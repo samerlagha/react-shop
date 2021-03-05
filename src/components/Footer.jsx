5ebb5a5f7ffb5038bb212283782e0347fd25d36b
Your Name <samerukr@gmail.com>
2021-03-05 11:14:14 +0200

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,13 @@ function Footer() {
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+      <a className="grey-text text-lighten-4 right"
+       href="https://github.com/samerlagha/react-shop"
+       target="_blank"
+       rel="noreferrer"
+       >
+         
+         Repo</a>
       </div>
     </div>
   </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,14 @@ function Header() {
         <div className="nav-wrapper">
         <a href="/" className="brand-logo">React Shop</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
+            <li>
+                <a href="https://github.com/samerlagha/react-shop" 
+                target="_blank"
+                rel="noreferrer"
+                >
+                    
+                    Repo</a>
+                    </li>
         </ul>
         </div>
     </nav>
